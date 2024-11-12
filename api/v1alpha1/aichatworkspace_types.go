f11932e51838a9a966950c06ec31900e2fb11799
Chauncey Thorn <chaunceyt@gmail.com>
2024-11-12 15:02:04 -0500

    chore: defining the aichatworkspace spec

--- api/v1alpha1/aichatworkspace_types.go
+++ api/v1alpha1/aichatworkspace_types.go
@@ -20,22 +20,27 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // AIChatWorkspaceSpec defines the desired state of AIChatWorkspace.
 type AIChatWorkspaceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// The name of the workspace.
+	WorkspaceName string `json:"workspaceName"`
 
-	// Foo is an example field of AIChatWorkspace. Edit aichatworkspace_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// List of default models for this workspace.
+	Models []string `json:"models"`
 }
 
 // AIChatWorkspaceStatus defines the observed state of AIChatWorkspace.
 type AIChatWorkspaceStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Represents the observations of a AIChatWorkspace's current state.
+	// AIChatWorkspace.status.conditions.type are: "Available", "Progressing", and "Degraded"
+	// AIChatWorkspace.status.conditions.status are one of True, False, Unknown.
+	// AIChatWorkspace.status.conditions.reason the value should be a CamelCase string and producers of specific
+	// condition types may define expected values and meanings for this field, and whether the values
+	// are considered a guaranteed API.
+	// AIChatWorkspace.status.conditions.Message is a human readable message indicating details about the transition.
+	// For further information see: https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
+
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type" protobuf:"bytes,1,rep,name=conditions"`
 }
 
 // +kubebuilder:object:root=true
